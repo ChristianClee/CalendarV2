b073c41388a07094bc25aeec2c1d0be02cf20c87
ChristianClee <ilya.mishkov@gmail.com>
2023-05-16 17:38:14 +0300

    .

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+
+const Header:React.FC = () => {
+  return (
+    <header className="header">
+      <div className="header__logo">
+        <div className="header__text">Interview Calendar</div>
+        <div className="header__img">+</div>
+      </div>
+    </header>
+  );
+}
+export default Header;
