0835a76054070b6a9596190383b57f3b8a84ca77
ChristianClee <ilya.mishkov@gmail.com>
2023-05-23 08:10:44 +0300

    new description message

--- src/components/popUpMessages/Description.tsx
+++ src/components/popUpMessages/Description.tsx
@@ -12,7 +12,6 @@ const Description: React.FC = () => {
       <div className='description__container'>
         <div className='description__burger-container' >
           <div
-            // className="description__burger checked"
             className={(burger) ? "description__burger checked" : "description__burger"}
             onClick={() => {
               setBurger(true)
@@ -25,34 +24,37 @@ const Description: React.FC = () => {
           </div>
         </div>
         <div className="description__boby">
-          <div className="description__title">Календарь</div>
-          <div className="description__text">тестовая работа</div>
 
-          <ul>Требования:
-            <li>сверстать страницу календаря</li>
-            <li>реализовать PopUp окно при нажатии на +</li>
-            <li>добавление событий</li>
-            <li></li>
-            <li></li>
+          <div className ="description__inner-body">
+            <div className="description__title">Календарь</div>
+            
+            <div className="description__subtitle">тестовая работа</div>
+            <div className="description__text">
+              эта работа предназначенна для мобильной версии,
+              откройте её через телефон или уменьшить
+              ширину окна браузера
+            </div>
 
-          </ul>
-
-          <ul>Реализованно:
-            <li>всё выше перечисленное </li>
-
-            <li>+ открывать новые страницы</li>
-            <li>+ все даты генерируются автоматически</li>
-            <li>+ правильность дат гарантированна</li>
-            <li>+ сохранять события на любых страницах</li>
-            <li>+ валидация при вводе с PoPUp</li>
-            <li>+ валидация не позволит ввести прошедшую дату </li>
-            <li>+ события сохраняются как от касания так и от ввода из PopUp</li>
-
-          </ul>
-          <div className="description__text"> Некоторые детали изменены намеренно (например дизайн кнопок).Pixel perfect вроде никто не требовал  </div>
+            
+            <ul> уже реализованно:
+              <li>открытие новых страниц (пока от кнопок)</li>
+              <li>все даты генерируются автоматически</li>
+              <li>правильность дат гарантированна</li>
+              <li>события сохраняются на любых страницах</li>
+              <li>валидация при вводе с PoPUp message</li>
+              <li>валидация не позволит ввести прошедшую дату </li>
+              <li>события сохраняются как от касания так и от ввода из PopUp</li>
+            </ul>
+          </div>
+          <div className="description__text">
+            работа не окончательна, находится в разработке и постоянно дополняется
+          </div>
+          
 
         </div>
+
       </div>
+      
     </div>
 
   );
